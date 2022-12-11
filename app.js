18fb4614fc4a558824b909a42107b1870ff4d5f1
Eduardo Pastrano <epastranom@gmail.com>
2022-12-11 13:43:51 -0500

    Segunda entrega

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
-let opciones = prompt(`Por favor, elige la operación que deseas realizar:
-1: Suma
-2: Resta
-3: Multiplicación
-4: División
+let opciones = prompt(`Por favor seleccione la opción que desee realizar:
+1: Monto total a pagar por productos ingresados.
+2: Disponibilidad de venta de una marca en una tienda de celulares.
 `);
 
 switch (opciones) {
     case "1":
-        alert("Vamos a realizar una suma");
-        let numeroSuma = parseInt(prompt("Ingrese un número o escriba '0' para salir."));
-        let resultadoSuma = 0;
+        alert("Vamos a agregar 5 productos a nuestro carrito y calcular el monto total a pagar.")
+        const listaProductos = [];
+        const precioProductos = [];
+        let montoTotal = 0;
+        let cantidad = 5;
 
-        while (numeroSuma != 0) {
-            resultadoSuma = resultadoSuma + numeroSuma;
-            numeroSuma = parseInt(prompt("Ingrese un número o escriba '0' para salir."));
-        }
-        alert(`El resultado de la suma es: ${resultadoSuma}`);
+        do {
+            let producto = prompt("Por favor ingrese el producto.");
+            listaProductos.push(producto.toLowerCase());
+            let precio = parseInt(prompt("Por favor ingrese el precio."));
+            precioProductos.push(precio);
+            montoTotal = montoTotal + precio;
+            } while (listaProductos.length != cantidad);
+            alert(`Los productos en tu carrito son: ${listaProductos.join(", ")}. Y los precios son: ${precioProductos.join(", ")}.`);
+            alert(`El monto total a pagar es: ${montoTotal}`);
         break;
-    
+
     case "2":
-        alert("Vamos a realizar una resta");
-        let numeroResta1 = parseInt(prompt("Ingrese el primer número."));
-        let numeroResta2 = parseInt(prompt("Ingrese el segundo número."));
-        let resultadoResta = numeroResta1 - numeroResta2;
+        const marcas = [
+            {nombre: 'Apple', stock: '15 telefonos en stock'},
+            {nombre: 'Samsung', stock: '25 telefonos en stock'},
+            {nombre: 'Xiaomi', stock: '20 telefonos en stock'}
+        ]
 
-        alert(`El resultado de la resta es: ${resultadoResta}`);
-        break;
+        const nombres = marcas.map((disponible) => {
+            return {
+                nombre: disponible.nombre,
+                stock: disponible.stock
+            }
+        });
 
-    case "3": 
-        alert("Vamos a realizar una multiplicación");
-        let numeroMultiplicacion = parseInt(prompt("Ingrese un número o escriba '0' para salir."));
-        let resultadoMultiplicacion = 1;
-
-        while (numeroMultiplicacion != 0) {
-            resultadoMultiplicacion = resultadoMultiplicacion * numeroMultiplicacion;
-            numeroMultiplicacion = parseInt(prompt("Ingrese un número o escriba '0' para salir."));
-        }
-        alert(`El resultado de la multiplicación es: ${resultadoMultiplicacion}`);
-        break;
-
-    case "4": 
-        alert("Vamos a realizar una división");
-        let numeroDivision1 = parseInt(prompt("Ingrese el primer número."));
-        let numeroDivision2 = parseInt(prompt("Ingrese el segundo número."));
-        
-        function division(numeroDivision1, numeroDivision2) {
-            return numeroDivision1 / numeroDivision2;
-        }
-
-        let resultadoDivision = division(numeroDivision1, numeroDivision2);
-
-        alert(`El resultado de la división es: ${resultadoDivision}`);
+        console.log(nombres);
         break;
 }
+
