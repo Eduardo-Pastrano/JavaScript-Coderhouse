f30edb4267f21f2539e591de6e3a8feec1b31c77
Eduardo Pastrano <epastranom@gmail.com>
2022-12-12 23:13:18 -0500

    Correcciones, re entrega 2.

--- app.js
+++ app.js
@@ -1,43 +1,74 @@
-let opciones = prompt(`Por favor seleccione la opción que desee realizar:
-1: Monto total a pagar por productos ingresados.
-2: Disponibilidad de venta de una marca en una tienda de celulares.
-`);
+const telefonos = [
+    { id: "1", nombre: "iPhone 14 Pro Max", precio: 1469 },
+    { id: "2", nombre: "Samsung Galaxy S22 Ultra", precio: 999 },
+    { id: "3", nombre: "One Plus 10 Pro", precio: 699 },
+    { id: "4", nombre: "Vivo X80 Pro", precio: 1199 },
+    { id: "5", nombre: "Xiaomi 12T Pro", precio: 779 },
+];
 
-switch (opciones) {
-    case "1":
-        alert("Vamos a agregar 5 productos a nuestro carrito y calcular el monto total a pagar.")
-        const listaProductos = [];
-        const precioProductos = [];
-        let montoTotal = 0;
-        let cantidad = 5;
+let carrito = [];
 
-        do {
-            let producto = prompt("Por favor ingrese el producto.");
-            listaProductos.push(producto.toLowerCase());
-            let precio = parseInt(prompt("Por favor ingrese el precio."));
-            precioProductos.push(precio);
-            montoTotal = montoTotal + precio;
-            } while (listaProductos.length != cantidad);
-            alert(`Los productos en tu carrito son: ${listaProductos.join(", ")}. Y los precios son: ${precioProductos.join(", ")}.`);
-            alert(`El monto total a pagar es: ${montoTotal}`);
-        break;
+let decidir = prompt("Bienvenido! Desea comprar algun producto?, por favor escriba 'si' o 'no'.")
 
-    case "2":
-        const marcas = [
-            {nombre: 'Apple', stock: '15 telefonos'},
-            {nombre: 'Samsung', stock: '25 telefonos'},
-            {nombre: 'Xiaomi', stock: '20 telefonos'},
-            {nombre: 'Motorola', stock: '10 telefonos'}
-        ]
-
-        const nombres = marcas.map((disponible) => {
-            return {
-                nombre: disponible.nombre,
-                stock: disponible.stock
-            }
-        });
-
-        console.log(nombres);
-        break;
+while(decidir != "si" && decidir != "no") {
+    alert("Por favor ingrese 'si' o 'no'.")
+    decidir = prompt("Bienvenido! Desea comprar algun producto?, por favor escriba 'si' o 'no'.")
 }
 
+if(decidir === "si"){
+    alert("Bienvenido! Disponemos de estos telefonos en stock")
+    let listadoTelefonos = telefonos.map((telefono) => telefono.id + ": " + telefono.nombre + " " + "$" + telefono.precio);
+    alert(listadoTelefonos.join("\n"));
+} else if (decidir === "no") {
+    alert("Gracias por su visita!")
+}
+
+while(decidir != "no"){
+    let telefono = prompt("Por favor agrega un producto a tu carrito")
+    let precio = 0
+
+    if(telefono == "1" || telefono == "2" || telefono == "3" || telefono == "4" || telefono == "5"){
+        switch(telefono) {
+            case "1":
+                nombre = "iPhone 14 Pro Max"
+                precio = 1469
+                break
+            case "2":
+                nombre = "Samsung Galaxy S22 Ultra"
+                precio = 999
+                break
+            case "3":
+                nombre = "One Plus 10 Pro"
+                precio = 699
+                break
+            case "4":
+                nombre = "Vivo X80 Pro"
+                precio = 1199
+                break
+            case "5":
+                nombre = "Xiaomi 12T Pro"
+                precio = 779
+                break
+            default:
+                break;
+        }
+        let cantidad = parseInt(prompt("¿Cuantos desea llevar?"))
+
+        carrito.push({nombre, cantidad, precio})
+        console.log(carrito)
+    } else {
+        alert("Disculpe no disponemos de ese telefono.")
+    }
+
+    decidir = prompt("¿Desea agregar mas telefonos a su carrito? Por favor, escriba 'si' o 'no.'")
+
+    while(decidir === "no"){
+        carrito.forEach((carritoFinal) => {
+            alert(`Telefono(s): ${carritoFinal.nombre}, cantidad: ${carritoFinal.cantidad}, total a pagar por telefono(s): $${carritoFinal.cantidad * carritoFinal.precio}`)
+        })
+    break
+    }
+}
+
+const montoTotal = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
+alert(`El total a pagar es: $${montoTotal}. Gracias por su compra, vuelva pronto!`);
