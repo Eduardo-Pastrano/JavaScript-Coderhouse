81724ba2c505bb9024cecdeca637f01a4c38ddb0
Eduardo Pastrano <epastranom@gmail.com>
2023-01-17 15:54:09 -0500

    Se realizan cambios al DOM, se modifica el CSS, se modifica el archivo JS para adaptarlo a la consigna final

--- js/index.js
+++ js/index.js
@@ -1,78 +1,62 @@
-let menu = document.querySelector(".menu-grilla");
+const menu = document.querySelector(".menu-grilla");
 
-fetch('./datos.json')
-    .then((response) => response.json())
-    .then((datos) => {
-        datos.forEach((producto) => {
-            let div = document.createElement("div");
-                div.innerHTML = `
-                <h4 class="nombre">${producto.nombre}</h4>
-                <h5 class="precio">$${producto.precio}</h5>
-                <button class="btn btn-success btn-add">Agregar</button>
-                `;
-                document.querySelector(".menu-grilla").appendChild(div);
+const solicitarInfo = async () => {
+    const response = await fetch('./js/data.json');
+    const data = await response.json();
+
+    data.forEach((producto) => {
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <img class="img-producto" src="${producto.img}">
+            <h4 class="nombre-producto">${producto.nombre}</h4>
+            <h5 class="precio-producto">$${producto.precio}</h5>
+            <button class="btn btn-success btn-add">Agregar</button>
+            `;
+        document.querySelector(".menu-grilla").appendChild(div);
+    });
+
+    let btnAdd = document.querySelectorAll(".btn-add");
+
+    btnAdd.forEach(boton => {
+        boton.addEventListener("click", (e) => {
+            datosProductos(e.target.parentElement);
         });
     });
 
-// let mostrarProductos = () => {
-//     localStorage.setItem("productos", JSON.stringify(productos))
-//     let listadoProductos = JSON.parse(localStorage.getItem("productos"))
+    let carrito = [];
 
-//     for (let index = 0; index < listadoProductos.length; index++) {
-//         let div = document.createElement("div");
-//         div.innerHTML = `
-//         <h4 class="nombre">${listadoProductos[index].nombre}</h4>
-//         <h5 class="precio">$${listadoProductos[index].precio}</h5>
-//         <button class="btn btn-success btn-add">Agregar</button>
-//         `;
-//         document.querySelector(".menu-grilla").appendChild(div);
-//     }
-// }
+    function datosProductos(producto) {
+        const informacion = {
+            nombre: producto.querySelector(".nombre-producto").textContent,
+            precio: producto.querySelector(".precio-producto").textContent,
+        };
+    
+        carrito=[...carrito, informacion]
+        pintarCarrito();
+    }
+    
+    const carritoFinal = document.querySelector(".productos-carrito");
+    
+    function pintarCarrito(){
+        resetPintarCarrito();
+    
+        carrito.forEach((producto)=>{
+            const divCarrito = document.createElement("div");
+            divCarrito.innerHTML=`
+            <img class="img-producto" src="${producto.img}">
+            <p>${producto.nombre}</p>
+            <p>${producto.precio}</p>
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" id="${producto.id}">Eliminar</button>
+            `;
+            document.querySelector(".productos-carrito").appendChild(divCarrito);
+        })
+    }
+    
+    function resetPintarCarrito(){
+        carritoFinal.innerHTML = "";
+    }
 
-if (localStorage.getItem("productos")) {
-    console.log("esta guardado");
-    mostrarProductos();
-} else {
-    console.log("no esta guardado");
-    localStorage.setItem("productos", JSON.stringify(productos))
-    mostrarProductos();
-}
 
-let btnAdd = document.querySelectorAll(".btn-add");
-
-btnAdd.forEach(boton => {
-    boton.addEventListener("click", (e) => {
-        datosProductos(e.target.parentElement);
-    });
-});
-
-let carrito = [];
-
-function datosProductos(producto) {
-    const informacion = {
-        nombre: producto.querySelector(".nombre").textContent,
-        precio: producto.querySelector(".precio").textContent,
-    };
-
-    carrito=[...carrito, informacion]
-    pintarCarrito();
-}
-
-const carritoFinal = document.querySelector(".info-carrito");
-
-function pintarCarrito(){
-    resetPintarCarrito();
-
-    carrito.forEach((producto)=>{
-        const divCarrito = document.createElement("div");
-        divCarrito.innerHTML=`
-        <p>${producto.nombre}</p>
-        <p>${producto.precio}</p>
-        `;
-        document.querySelector(".info-carrito").appendChild(divCarrito);
-    })
-}
-
-function resetPintarCarrito(){
-    carritoFinal.innerHTML = "";
-}
+};
+solicitarInfo();
