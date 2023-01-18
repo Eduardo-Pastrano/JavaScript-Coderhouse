7176298fb553bd6dd788edab9d816028d9ccdab5
Eduardo Pastrano <epastranom@gmail.com>
2023-01-18 13:36:54 -0500

    Se modifica elementos de objetos en el JSON, se finaliza funcionalidad de boton agregar y eliminar, funcion para sumar el total del carrito al agregar o quitar productos terminada. Se agrega el formulario por primera vez.

--- js/index.js
+++ js/index.js
@@ -4,13 +4,14 @@ const solicitarInfo = async () => {
     const response = await fetch('./js/data.json');
     const data = await response.json();
 
-    data.forEach((producto) => {
+    data.forEach((postre) => {
         let div = document.createElement("div");
+        div.className = "postres"
         div.innerHTML = `
-            <img class="img-producto" src="${producto.img}">
-            <h4 class="nombre-producto">${producto.nombre}</h4>
-            <h5 class="precio-producto">$${producto.precio}</h5>
-            <button class="btn btn-success btn-add">Agregar</button>
+            <img class="img-postre" src="${postre.img}">
+            <span class="nombre-postre">${postre.nombre}</span>
+            <h5 class="precio-postre">$${postre.precio}</h5>
+            <button class="btn btn-success btn-add" id="${postre.id}">Agregar</button>
             `;
         document.querySelector(".menu-grilla").appendChild(div);
     });
@@ -25,38 +26,97 @@ const solicitarInfo = async () => {
 
     let carrito = [];
 
-    function datosProductos(producto) {
+    function datosProductos(postre) {
         const informacion = {
-            nombre: producto.querySelector(".nombre-producto").textContent,
-            precio: producto.querySelector(".precio-producto").textContent,
+            nombre: postre.querySelector(".nombre-postre").textContent,
+            precio: postre.querySelector(".precio-postre").textContent,
+            img: postre.querySelector(".img-postre").src,
+            id: postre.querySelector(".btn").getAttribute("id")
         };
     
         carrito=[...carrito, informacion]
         pintarCarrito();
     }
+
+    const carritoFinal = document.querySelector(".postres-carrito");
     
-    const carritoFinal = document.querySelector(".productos-carrito");
-    
-    function pintarCarrito(){
+    function pintarCarrito() {
         resetPintarCarrito();
-    
-        carrito.forEach((producto)=>{
-            const divCarrito = document.createElement("div");
+
+        carrito.forEach((postre) => {
+            let divCarrito = document.createElement("div");
+            divCarrito.className = "fila-carrito"
             divCarrito.innerHTML=`
-            <img class="img-producto" src="${producto.img}">
-            <p>${producto.nombre}</p>
-            <p>${producto.precio}</p>
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" id="${producto.id}">Eliminar</button>
+                <div class="postre-carrito columna-carrito">
+                    <img class="img-postre-carrito" src="${postre.img}">
+                    <p class="postre-nombre">${postre.nombre}</p>
+                </div>
+                <p class="precio-carrito columna-carrito">${postre.precio}</p>
+                <div class="cantidad-carrito columna-carrito">
+                    <input class="cantidad-carrito-input" type="number" value="1">
+                    <button class="btn btn-danger" id="${postre.id}">Eliminar</button>
+                </div>
             `;
-            document.querySelector(".productos-carrito").appendChild(divCarrito);
-        })
+            document.querySelector(".postres-carrito").appendChild(divCarrito);
+
+            carritoFinal.getElementsByClassName("cantidad-carrito-input")[0].addEventListener("change", cambioCantidad);
+            carritoFinal.addEventListener("click", eliminarItem);
+            document.getElementsByClassName("btn-comprar")[0].addEventListener("click", btnComprar);
+            actTotalCarrito();
+
+            let inputCantidad = document.getElementsByClassName("cantidad-carrito-input")
+            for (let i = 0; i < inputCantidad.length; i++) {
+                let input = inputCantidad[i]
+                input.addEventListener("change", cambioCantidad)
+            }
+        });
     }
-    
+
+    function btnComprar() {
+        alert("Gracias por su compra!")
+        let postresCarrito = document.getElementsByClassName("postres-carrito")[0]
+        while (postresCarrito.hasChildNodes()) {
+            postresCarrito.removeChild(postresCarrito.firstChild)
+        }
+        actTotalCarrito();
+    }
+
+    function actTotalCarrito() {
+        let contenedorItemCarrito = document.getElementsByClassName("postres-carrito")[0]
+        let filasCarrito = contenedorItemCarrito.getElementsByClassName("fila-carrito")
+        let total = 0
+        for (let i = 0; i < filasCarrito.length; i++) {
+            let filaCarrito = filasCarrito[i]
+            let precioPostre = filaCarrito.getElementsByClassName("precio-carrito")[0]
+            let cantidadPostre = filaCarrito.getElementsByClassName("cantidad-carrito-input")[0]
+            let precio = parseFloat(precioPostre.innerText.replace("$", ''))
+            let cantidad = cantidadPostre.value
+            total = total + (precio * cantidad)
+        }
+        document.getElementsByClassName("carrito-total-precio")[0].innerText = "$" + total
+    }
+
+    function cambioCantidad(e) {
+        let input = e.target
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1
+        }
+        actTotalCarrito();
+    }
+
+    function eliminarItem(e) {
+        if (e.target.classList.contains("btn-danger")) {
+        let postreID = e.target.getAttribute("id");
+        carrito = carrito.filter(
+            (postre) => postre.id !== postreID
+        );
+        pintarCarrito();
+        actTotalCarrito();
+        }
+    }
+
     function resetPintarCarrito(){
         carritoFinal.innerHTML = "";
     }
-
-
 };
 solicitarInfo();
